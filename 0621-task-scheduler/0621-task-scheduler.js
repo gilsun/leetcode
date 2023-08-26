f8a94da2c4ca127294bc2f0dfdd71fdb0d68033d
Gilsun Yu <gilsun0821@gmail.com>
2023-08-25 20:39:59 -0700

    Time: 96 ms (63.29%), Space: 46.6 MB (69.93%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -3,34 +3,33 @@
  * @param {number} n
  * @return {number}
  */
-// ASCII (American Standard Code for Information Interchange)에서는 대문자 알파벳의 유니코드
-// A: 65
-// B: 66
-// C: 67
-// D: 68
-// E: 69
-// ...
-// Z: 90
-const leastInterval = function(tasks, n) {
-  if (n === 0) return tasks.length;
-
-     let map = {}; 
+var leastInterval = function(tasks, n) {
+    if (n === 0) return tasks.length
+    
+    let map = {}
+    
     for (const task of tasks) {
         if (!map[task]) {
-            map[task] = 0;
+            map[task] = 0; 
         }
-        map[task]++;
+        map[task]++
     }
+    
+    console.log('map', map)
+    let counts = Object.values(map)
+ 
+    counts.sort((a,b) => b - a)
+    console.log('counts', counts )
+    
+    let most = counts[0]
+    let idle = (most - 1)*n 
 
- const counts = Object.values(map);
-  counts.sort((a, b) => b - a);
-  const most = counts[0];
-  let idle = (most - 1) * n;
 
-  for (let i = 1; i < counts.length; i++) {
-      idle -= Math.min(counts[i], most - 1, idle);
-      if (idle === 0) break;
-  }
-
-  return tasks.length + idle;
+    for (let i = 1; i < counts.length; i++) {
+         idle -= Math.min(counts[i], most - 1, idle);
+        if (idle === 0) break 
+    }
+    
+    return tasks.length + idle 
+    
 };
