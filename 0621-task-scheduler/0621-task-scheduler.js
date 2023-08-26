c878a76b4baa325d0917c19fbfbd7e69873c31bf
Gilsun Yu <gilsun0821@gmail.com>
2023-08-25 18:39:04 -0700

    Time: 76 ms (91.96%), Space: 45.6 MB (90.56%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -11,26 +11,43 @@
 // E: 69
 // ...
 // Z: 90
-var leastInterval = function(tasks, n) {
-   let taskCount = new Array(26).fill(0);
-   
-   for (let task of tasks) {
-       let index = task.charCodeAt() - "A".charCodeAt();
-       taskCount[index]++;
-   }
-    
-    taskCount.sort((a,b) => a - b);
-    
-    let mostFrequentTaskCount = taskCount[25];
-   
-    let totalIdleCount = (mostFrequentTaskCount - 1) * n;
-    
-    for (let i = 24; i >= 0 && taskCount[i] > 0; i--) {
-        totalIdleCount = totalIdleCount - Math.min(taskCount[i], mostFrequentTaskCount - 1);
-    }
-    
-    if (totalIdleCount > 0) return totalIdleCount + tasks.length;
-    else return tasks.length;
-};
-    
+const leastInterval = function(tasks, n) {
+  // 작업 간의 간격이 필요하지 않을 경우, 총 작업 시간은 작업의 개수와 같습니다.
+  if (n === 0) return tasks.length;
 
+  // 각 작업의 빈도를 계산합니다.
+  const counts = Array.from(counter(tasks).values());
+
+  // counts를 내림차순으로 정렬합니다. (최대 26개의 알파벳만 존재하므로 O(1) 복잡도입니다.)
+  counts.sort((a, b) => b - a);
+
+  // 가장 빈번하게 등장하는 작업의 빈도입니다.
+  const most = counts[0];
+  
+  // 가장 빈번한 작업 사이에 필요한 idle(대기) 시간을 계산합니다.
+  let idle = (most - 1) * n;
+
+  // 두 번째로 많이 등장하는 작업부터, 빈번한 작업 사이에 넣을 수 있는 작업의 수를 계산합니다.
+  for (let i = 1; i < counts.length; i += 1) {
+    // 남아있는 대기 시간에서 현재 작업 빈도(또는 most-1, 남아있는 idle 중 작은 것)를 빼줍니다.
+    idle -= Math.min(counts[i], most - 1, idle);
+    
+    // 더 이상의 대기 시간이 필요하지 않을 경우 반복을 종료합니다.
+    if (idle === 0) break;
+  }
+
+  // 총 작업 시간을 반환합니다. (실제 작업 + 대기 시간)
+  return tasks.length + idle;
+};
+
+// 각 작업의 빈도를 계산하는 도우미 함수입니다.
+const counter = (tasks) => {
+  const map = new Map();
+
+  // 각 작업의 빈도를 계산하여 맵에 저장합니다.
+  for (const task of tasks) {
+    map.set(task, (map.get(task) || 0) + 1);
+  }
+
+  return map;
+}
