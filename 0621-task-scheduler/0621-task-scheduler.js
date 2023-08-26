79452c1542055c85d5b045d3400a5a2f90f0ae23
Gilsun Yu <gilsun0821@gmail.com>
2023-08-25 21:38:53 -0700

    Time: 88 ms (74.82%), Space: 46.9 MB (62.94%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -3,31 +3,35 @@
  * @param {number} n
  * @return {number}
  */
+// ["A","A","A","B","B","C", "C", "D"]
+// 2
+
 var leastInterval = function(tasks, n) {
-    if (n === 0) return tasks.length
+    
+    if (n === 0 ) return tasks.length
     
     let map = {}
     
     for (const task of tasks) {
-        if (!map[task]) {
-            map[task] = 0; 
+        if(!map[task]){
+            map[task] = 0
         }
         map[task]++
     }
     
-    let counts = Object.values(map)
-
-    counts.sort((a,b) => b - a)
-
+    console.log(map)
+    
+    const counts = Object.values(map)
+    // [3,2,2,1]
+    counts.sort((a,b) => b - a) 
     
     let most = counts[0]
-    let idle = (most - 1)*n 
-
-    for (let i = 1; i < counts.length; i++) {
-         idle = idle-Math.min(counts[i], most - 1, idle);
-       
+    let slot = (most - 1)*n  
+    
+    for (let i = 1; i < counts.length; i ++) {
+        slot = slot - Math.min(counts[i], most - 1, slot)
     }
     
-    return tasks.length + idle 
+    return slot + tasks.length 
     
 };
