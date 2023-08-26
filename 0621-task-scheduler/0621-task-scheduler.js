0d73a61272f870794dad9194e8ce5eca77b46825
Gilsun Yu <gilsun0821@gmail.com>
2023-08-25 20:51:22 -0700

    Time: 99 ms (59.09%), Space: 44.9 MB (98.95%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.js
+++ 0621-task-scheduler/0621-task-scheduler.js
@@ -23,10 +23,9 @@ var leastInterval = function(tasks, n) {
     let most = counts[0]
     let idle = (most - 1)*n 
 
-
     for (let i = 1; i < counts.length; i++) {
          idle = idle-Math.min(counts[i], most - 1, idle);
-        if (idle === 0) break 
+       
     }
     
     return tasks.length + idle 
