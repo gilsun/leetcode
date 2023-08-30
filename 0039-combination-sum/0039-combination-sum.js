35dd1ea3982585b80188cfffd2b736da3df36a45
Gilsun Yu <gilsun0821@gmail.com>
2023-08-29 19:51:48 -0700

    Time: 70 ms (65.96%), Space: 44.7 MB (91.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.js
+++ 0039-combination-sum/0039-combination-sum.js
@@ -6,20 +6,17 @@
 var combinationSum = function(candidates, target) {
     let result = [];
     
-    // 먼저 candidates를 오름차순 정렬
-    candidates.sort((a, b) => a - b);
-    
     const backtrack = (currentCombination, startIndex, currentTarget) => {
         // 종료 조건: 현재 목표값이 0이면 결과에 추가
         if (currentTarget === 0) {
             result.push([...currentCombination]);
             return;
         }
-        
-        for (let i = startIndex; i < candidates.length; i++) {
-            // 현재 후보가 현재 목표보다 클 경우, 이후의 후보는 모두 크기 때문에 반복을 중단합니다.
-            if (candidates[i] > currentTarget) break;
 
+        // 종료 조건: 현재 목표값이 음수이면 반복 중지
+        if (currentTarget < 0) return;
+
+        for (let i = startIndex; i < candidates.length; i++) {
             // 현재 후보를 조합에 추가하고 백트래킹을 시작합니다.
             currentCombination.push(candidates[i]);
             backtrack(currentCombination, i, currentTarget - candidates[i]);
@@ -29,6 +26,7 @@ var combinationSum = function(candidates, target) {
     
     // 백트래킹 시작
     backtrack([], 0, target);
-    
+
     return result;
 };
+    
