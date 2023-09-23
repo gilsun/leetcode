302974f4549f3809f8bc9e1d4ad2ba6e86dbba14
Gilsun Yu <gilsun0821@gmail.com>
2023-09-22 20:24:23 -0700

    Time: 62 ms (33.56%), Space: 43.8 MB (30.93%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    let res = Math.max(...nums)
+    let curMin = 1
+    let curMax = 1 
+    
+    for ( let num of nums) {
+        if (num === 0) {
+            curMin = 1
+            curMax = 1
+            continue 
+        }
+        let temp = curMin * num 
+        curMin = Math.min(num * curMin, num * curMax, num)
+        curMax = Math.max( num * curMax, temp, num)
+        
+        res = Math.max(res, curMax)
+    }
+    
+    return res 
+
+};
