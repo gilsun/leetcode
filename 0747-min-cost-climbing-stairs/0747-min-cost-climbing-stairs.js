a72768ee141e4636ff21b71e013beef85443bcb3
Gilsun Yu <gilsun0821@gmail.com>
2023-10-20 01:47:26 -0700

    Time: 64 ms (16.3%), Space: 42.3 MB (84.15%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -4,18 +4,14 @@
  */
 var minCostClimbingStairs = function(cost) {
 
-
-
      const n = cost.length;
-    let dp = Array(n).fill(0);
+    let dp = Array(n+1).fill(0);
 
-    // 첫 번째 및 두 번째 계단을 무료로 오를 수 있으므로 dp[0]과 dp[1]을 0으로 초기화
     dp[0] = 0;
     dp[1] = 0;
 
-    for (let i = 2; i < n; i++) {
+    for (let i = 2; i <= n; i++) {
         dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
     }
-
-    return Math.min(dp[n - 2] + cost[n - 2], dp[n - 1] + cost[n - 1]);
+    return dp[n]
 };
