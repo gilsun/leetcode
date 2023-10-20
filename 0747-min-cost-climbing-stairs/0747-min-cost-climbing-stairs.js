1c285d1cee8f001797ee5767f020daf50f596b10
Gilsun Yu <gilsun0821@gmail.com>
2023-10-20 01:42:22 -0700

    Time: 63 ms (20.08%), Space: 43.3 MB (58.87%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.js
@@ -3,15 +3,6 @@
  * @return {number}
  */
 var minCostClimbingStairs = function(cost) {
-    // const n = cost.length
-    // let dp = Array(n).fill(0)
-    // dp[0] = 0
-    // dp[1] = 0
-    // for (let i = 2; i < n; i ++ ) {
-    //    dp[i] = Math.min(dp[i-2]+ cost[i], dp[i - 1] + cost[i])
-    // }
-
-    // return dp[n-1]
 
 
 
