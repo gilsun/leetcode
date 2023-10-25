fbec57405ba249b646a6de8e25cbbde836912ec8
Gilsun Yu <gilsun0821@gmail.com>
2023-10-24 17:36:38 -0700

    Time: 62 ms (66.27%), Space: 43.8 MB (89.97%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,37 +4,26 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
+    const res = [];
+    let i = 0;
 
-    const isOverLapping = (a,b)=>{
-        return a[1] >= b[0]
-    } 
-
-    const mergeInterval = (a,b) =>{
-        return [ Math.min(a[0], b[0]), Math.max(a[1], b[1])]
+    while (i < intervals.length && intervals[i][1] < newInterval[0]) {
+        res.push(intervals[i]);
+        i++;
     }
 
-    const res = [ ]
-    let i = 0 
-
-    while ( i < intervals.length  && intervals[i][1] < newInterval[0]) {
-        res.push(intervals[i])
-        i ++ 
+    while (i < intervals.length && newInterval[1] >= intervals[i][0]) {
+        newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+        newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+        i++;
     }
 
+    res.push(newInterval);
 
-    while ( i < intervals.length && isOverLapping(newInterval, intervals[i])) {
-        newInterval = mergeInterval (newInterval, intervals[i])
-        i++
+    while (i < intervals.length) {
+        res.push(intervals[i]);
+        i++;
     }
 
-    res.push(newInterval)
-
-    while ( i < intervals.length) {
-        res.push(intervals[i])
-        i ++
-    }
-
-    return res 
-
-
+    return res;
 };
