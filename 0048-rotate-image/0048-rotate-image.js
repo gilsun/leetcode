eac5620e76b23f04163ca10cc459ca32656940a7
Gilsun Yu <gilsun0821@gmail.com>
2023-10-24 21:23:39 -0700

    Time: 43 ms (94.04%), Space: 41.7 MB (80.68%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.js
+++ 0048-rotate-image/0048-rotate-image.js
@@ -25,19 +25,17 @@ reverse
             ]
 
     `
-   const n = matrix.length 
 
-   for (let i = 0; i < n; i ++) {
-       for (let j = i; j < n; j ++) {
-           [matrix[i][j], matrix[j][i]] =  [matrix[j][i],matrix[i][j]]
-       }
-   } 
+ const n = matrix.length 
 
-   for (let i = 0; i < n; i ++) {
-       matrix[i].reverse()
-   }
+ for ( let i = 0; i < n ; i ++) {
+     for (let j = i ; j < n; j ++ ) {
+         [matrix[i][j], matrix[j][i]] =[matrix[j][i],matrix[i][j]]
+     }
+ }
 
-
-
-
+for ( let i = 0; i < n ; i ++) {
+    matrix[i].reverse()
+}
+return matrix 
 };
