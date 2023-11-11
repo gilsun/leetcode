173ec9ee72a651b1a7bc3097ef0add187bd89240
Gilsun Yu <gilsun0821@gmail.com>
2023-11-10 17:51:24 -0800

    Time: 54 ms (38.59%), Space: 42 MB (29.87%) - LeetHub

--- 0343-integer-break/0343-integer-break.js
+++ 0343-integer-break/0343-integer-break.js
@@ -3,15 +3,30 @@
  * @return {number}
  */
 function integerBreak(n) {
+    const dpMemo = {};
+
+    function dp(num) {
+        if (num <= 3) {
+            return num;
+        }
+
+        if (dpMemo[num]) {
+            return dpMemo[num];
+        }
+
+        let ans = num;
+        for (let i = 2; i < num; i++) {
+            ans = Math.max(ans, i * dp(num - i));
+        }
+
+        dpMemo[num] = ans;
+
+        return ans;
+    }
+
     if (n <= 3) {
         return n - 1;
     }
 
-    let ans = 1;
-    while (n > 4) {
-        ans *= 3;
-        n -= 3;
-    }
-
-    return ans * n;
+    return dp(n);
 }
