8b171e1e93fe8cb9c65ef208393214b1a1256455
Gilsun Yu <gilsun0821@gmail.com>
2023-11-11 07:51:17 -0800

    Time: 63 ms (63.97%), Space: 46.8 MB (86.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -11,26 +11,25 @@
  * @return {boolean}
  */
 var isBalanced = function(root) {
-    // 재귀 함수를 정의합니다. 이 함수는 노드의 높이를 반환하거나, 균형이 깨졌을 때 -1을 반환합니다.
-    const checkBalance = (root) => {
-        if (root === null) {
-            return 0; // 빈 노드의 높이는 0입니다.
-        }
-
-        // 왼쪽과 오른쪽 서브트리의 높이를 계산합니다.
-        let leftHeight = checkBalance(root.left);
-        let rightHeight = checkBalance(root.right);
-
-        // 균형이 깨진 경우, -1을 전파합니다.
-        if (leftHeight === -1 || rightHeight === -1 || Math.abs(leftHeight - rightHeight) > 1) {
+     const height=(root)=>  {
+        // 빈 트리의 높이는 -1입니다.
+        if (!root) {
             return -1;
         }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
 
-        // 해당 노드의 높이는 자식 노드들 중 더 높은 높이에 1을 더한 값입니다.
-        return Math.max(leftHeight, rightHeight) + 1;
-    };
 
-    // 루트 노드에서 시작하여 트리의 균형 상태를 확인합니다.
-    return checkBalance(root) !== -1;
-};
+        // 빈 트리는 균형 트리의 정의를 만족합니다.
+        if (!root) {
+            return true;
+        }
+
+        // 서브트리의 높이가 1 이하 차이 나는지 확인하고, 만약 그렇다면
+        // 서브트리들도 균형 트리인지 확인합니다.
+        return Math.abs(height(root.left) - height(root.right)) < 2 
+            && isBalanced(root.left) 
+            && isBalanced(root.right);
+    }
+
 
