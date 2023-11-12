b7d5bc8d38efc468fe9f6ca40c7cf057af28948c
Gilsun Yu <gilsun0821@gmail.com>
2023-11-12 07:24:08 -0800

    Time: 67 ms (41.36%), Space: 48.2 MB (21.25%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.js
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -10,7 +10,7 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function(root) {
+ isBalanced = function(root) {
      const height=(root)=>  {
         // 빈 트리의 높이는 -1입니다.
         if (!root) {
@@ -19,17 +19,13 @@ var isBalanced = function(root) {
         return 1 + Math.max(height(root.left), height(root.right));
     }
 
-
         // 빈 트리는 균형 트리의 정의를 만족합니다.
         if (!root) {
             return true;
         }
-
         // 서브트리의 높이가 1 이하 차이 나는지 확인하고, 만약 그렇다면
         // 서브트리들도 균형 트리인지 확인합니다.
         return Math.abs(height(root.left) - height(root.right)) < 2 
             && isBalanced(root.left) 
             && isBalanced(root.right);
-    }
-
-
+}
