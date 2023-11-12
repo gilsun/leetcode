8ff307b80b997b49cdb05776b819c5790f46d92d
Gilsun Yu <gilsun0821@gmail.com>
2023-11-12 08:04:20 -0800

    Time: 52 ms (52.4%), Space: 42.4 MB (21.8%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -13,14 +13,43 @@
  */
 var isSameTree = function(p, q) {
 
-    if(p === null && q === null ) return true
-    if(p === null || q === null ) return false
-    if (p.val !== q.val){
-        return false 
-    }
+        // 두 노드를 확인하는 함수
+        function check(p, q) {
+            // 두 노드가 모두 null이면 true 반환
+            if (!p && !q) {
+                return true;
+            }
+            // 한쪽 노드만 null이면 false 반환
+            if (!p || !q) {
+                return false;
+            }
+            // 두 노드의 값이 다르면 false 반환
+            if (p.val !== q.val) {
+                return false;
+            }
+            return true;
+        }
 
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
+        // 큐 초기화 (JavaScript의 배열을 큐처럼 사용)
+        let queue = [[p, q]];
+
+        while (queue.length > 0) {
+            let [p, q] = queue.shift();
+            
+            if (!check(p, q)) {
+                return false;
+            }
+
+            if (p) {
+                queue.push([p.left, q.left]);
+                queue.push([p.right, q.right]);
+            }
+        }
+
+        return true;
 };
+
+
 
 
 // function isSameTree(p, q) {
