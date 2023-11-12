33e1117eb862c497a12841473e2d818a21436029
Gilsun Yu <gilsun0821@gmail.com>
2023-11-12 08:16:10 -0800

    Time: 52 ms (52.4%), Space: 42.2 MB (54.66%) - LeetHub

--- 0100-same-tree/0100-same-tree.js
+++ 0100-same-tree/0100-same-tree.js
@@ -11,60 +11,56 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
+// var isSameTree = function(p, q) {
 
-        // 두 노드를 확인하는 함수
-        function check(p, q) {
-            // 두 노드가 모두 null이면 true 반환
-            if (p === null && q === null) {
-                return true;
-            }
-            // 한쪽 노드만 null이면 false 반환
-            if (p === null || q === null) {
-                return false;
-            }
-            // 두 노드의 값이 다르면 false 반환
-            if (p.val !== q.val) {
-                return false;
-            }
-            return true;
-        }
+//         // 두 노드를 확인하는 함수
+//         function check(p, q) {
+//             // 두 노드가 모두 null이면 true 반환
+//             if (!p && !q ) {
+//                 return true;
+//             }
+//             // 한쪽 노드만 null이면 false 반환
+//             if (p === null || q === null) {
+//                 return false;
+//             }
+//             // 두 노드의 값이 다르면 false 반환
+//             if (p.val !== q.val) {
+//                 return false;
+//             }
+//             return true;
+//         }
 
-        // 큐 초기화 (JavaScript의 배열을 큐처럼 사용)
-        let queue = [[p, q]];
+//         // 큐 초기화 (JavaScript의 배열을 큐처럼 사용)
+//         let queue = [[p, q]];
 
-        while (queue.length > 0) {
-            let [p, q] = queue.shift();
+//         while (queue.length > 0) {
+//             let [p, q] = queue.shift();
             
-            if (!check(p, q)) {
-                return false;
-            }
+//             if (!check(p, q)) {
+//                 return false;
+//             }
 
-            if (p) {
-                queue.push([p.left, q.left]);
-                queue.push([p.right, q.right]);
-            }
-        }
+//             if (p) {
+//                 queue.push([p.left, q.left]);
+//                 queue.push([p.right, q.right]);
+//             }
+//         }
 
-        return true;
-};
-
-
-
-
-// function isSameTree(p, q) {
-//     // 두 노드가 모두 null이면 true 반환
-//     if (p === null && q === null) {
 //         return true;
-//     }
-//     // 한쪽만 null이면 false 반환
-//     if (p === null || q === null) {
-//         return false;
-//     }
-//     // 두 노드의 값이 다르면 false 반환
-//     if (p.val !== q.val) {
-//         return false;
-//     }
-//     // 왼쪽과 오른쪽 자식 트리를 재귀적으로 비교
-//     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-// }
+// };
+
+
+
+
+var isSameTree = (p, q) => {
+    if (p === null && q === null) {
+        return true;
+    }
+    if (p === null || q === null) {
+        return false;
+    }
+    if (p.val !== q.val) {
+        return false;
+    }
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
