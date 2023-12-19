a9dc61ea6dfe05642c4487abcb98d8f4a366f311
Gilsun Yu <gilsun0821@gmail.com>
2023-12-19 02:03:43 -0800

    Update 0036-valid-sudoku.js

--- 0036-valid-sudoku/0036-valid-sudoku.js
+++ 0036-valid-sudoku/0036-valid-sudoku.js
@@ -2,6 +2,8 @@
  * @param {character[][]} board
  * @return {boolean}
  */
+
+
 var isValidSudoku = function(board) {
   
     // set 
@@ -11,25 +13,34 @@ var isValidSudoku = function(board) {
     // check r c 
     // add to set 
 
-    let seen = new Set()
+    let seenRow = new Set()
+    let seenCol = new Set()
+    let seenSquare = new Set()
 
-    for (let r = 0; r < 9; r++ ) {
-        for (let c = 0; c < 9; c ++) {
-            let num = board[r][c]
-            if(num ==='.'){
-                continue 
+    let row = board.length 
+    let col = board[0].length 
+
+    for (let r = 0; r < row ; r ++){
+        for (let c = 0; c < col; c ++){
+            let boardNum = board[r][c]
+            let rowNum = `row num-${r} boardNum-${boardNum}`
+            let colNum = `col num-${c} boardNum-${boardNum}`
+            let square = `square- ${Math.floor(r/3)}${Math.floor(c/3)} boardNum${boardNum}`
+            if(boardNum ==="."){
+                continue
             }
-            let row = `row-${r}-${num}`
-            let col = `col-${c}-${num}`
-            let square = `square-${Math.floor(r/3)}-${Math.floor(c/3)}-${num} `
-            if(seen.has(row) || seen.has(col) || seen.has(square)){
+            if(seenRow.has(rowNum) || seenCol.has(colNum) || seenSquare.has(square)) {
                 return false 
             }
-            seen.add(row)
-            seen.add(col)
-            seen.add(square)
+            seenRow.add(rowNum)
+            seenCol.add(colNum)
+            seenSquare.add(square)
+
         }
     }
-    console.log('seen', seen)
-    return true
+    console.log('seenRow', seenRow)
+    console.log('seenCol', seenCol)
+    console.log('square', seenSquare)
+
+    return true 
 }
