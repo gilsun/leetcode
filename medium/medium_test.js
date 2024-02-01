dfbde3aefda1b0616c26ace9e7347c957c73aff6
gilsun <gilsun0821@gmail.com>
2024-02-01 03:58:51 -0800

    delete some code

--- medium/medium_test.js
+++ medium/medium_test.js
@@ -15,26 +15,6 @@ var verticalOrder = function (root) {
     return [];
   }
 
-  while (queue.length > 0) {
-    let [x, node] = queue.shift();
-
-    if (!columnItems[x]) {
-      columnItems[x] = [];
-    }
-
-    columnItems[x].push(node.val);
-
-    min_x = Math.min(min_x, x);
-    max_x = Math.max(max_x, x);
-
-    if (node.left) {
-      queue.push([x - 1, node.left]);
-    }
-    if (node.right) {
-      queue.push([x + 1, node.right]);
-    }
-  }
-
   for (let i = min_x; i <= max_x; i++) {
     res.push(columnItems[i]);
   }
